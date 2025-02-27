7ebb705807a87ed56a60982e532bd2c81a3057c0
Borislav Petrovski <petrovskiborislav@gmail.com>
2025-02-27 23:51:30 +0200

    setup gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base : '/treasure-hunt',
     plugins: [
         tailwindcss(),
         react(),
